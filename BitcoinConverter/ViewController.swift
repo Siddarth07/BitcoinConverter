b6df84d83c7530496c4b024fce4ed4ab8d7e107a
Siddarth Kethireddy <siddarthkethireddy@WTA-MBP273-Intern.local>
2017-05-30 13:55:36 -0400

    Initial Commit

---
+++ BitcoinConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinConverter
+//
+//  Created by Siddarth Kethireddy on 5/30/17.
+//  Copyright © 2017 Siddarth Kethireddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
